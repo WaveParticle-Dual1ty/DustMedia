3a94f48bf4aaf9e26908a9a8d711acad2a1dd2aa
Laplace <142323522+WaveParticle-Dual1ty@users.noreply.github.com>
2025-01-29 12:14:56 +0800

    TestLayer set dockspace

--- Source/Sandbox/TestLayer.h
+++ Source/Sandbox/TestLayer.h
@@ -21,6 +21,10 @@ public:
     virtual void OnEvent(ME::Event& event) override;
 
 private:
+    void BeginDockspace();
+    void EndDockspace();
+
+private:
     Ref<RHI> m_RHI;
     Ref<TestRenderPass> m_TestRenderPass;
     std::array<uint32_t, 2> m_ViewportSize = {0, 0};
