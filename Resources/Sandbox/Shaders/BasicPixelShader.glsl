098841496113e35658dc3289786d01fd189d4dc0
Laplace <142323522+WaveParticle-Dual1ty@users.noreply.github.com>
2025-01-31 20:20:52 +0800

    修改BasicPixelShader

--- Resources/Sandbox/Shaders/BasicPixelShader.glsl
+++ Resources/Sandbox/Shaders/BasicPixelShader.glsl
@@ -6,7 +6,7 @@ layout(location = 1) in vec2 VS_Texcoord;
 layout(push_constant) uniform PushConsts
 {
     vec4 Color;
-} g_pushConsts;
+} g_PushConsts;
 
 layout(binding = 0) uniform sampler2D g_TextureSampler;
 
@@ -14,7 +14,5 @@ layout(location = 0) out vec4 g_OutColor;
 
 void main()
 {
-    //g_OutColor = vec4(1, 0, 0, 1);
-    //g_OutColor = g_pushConsts.Color;
-    g_OutColor = texture(g_TextureSampler, VS_Texcoord);
+    g_OutColor = texture(g_TextureSampler, VS_Texcoord) * g_PushConsts.Color;
 }
