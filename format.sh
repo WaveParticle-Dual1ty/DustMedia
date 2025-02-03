025e5b187806b8db907e8b8ae9c9984947aac733
Laplace <142323522+WaveParticle-Dual1ty@users.noreply.github.com>
2025-02-03 12:27:06 +0800

    add ExVulkan

--- format.sh
+++ format.sh
@@ -1,4 +1,5 @@
 echo -e "--- start encode conv ---"
+python ./Scripts/Format/EncodeConv.py Source/Example
 python ./Scripts/Format/EncodeConv.py Source/MediaEngine
 python ./Scripts/Format/EncodeConv.py Source/EmptyApp
 python ./Scripts/Format/EncodeConv.py Source/Sandbox
@@ -8,6 +9,7 @@ echo -e "--- finish encode conv ---"
 echo -e "\n"
 
 echo -e "--- start clang-format ---"
+python ./Scripts/Format/FormatFile.py Source/Example
 python ./Scripts/Format/FormatFile.py Source/MediaEngine
 python ./Scripts/Format/FormatFile.py Source/EmptyApp
 python ./Scripts/Format/FormatFile.py Source/Sandbox
